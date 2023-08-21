ac3af3f899c8fe7fa0162cfaabafd120ff930d92
TOHA262000 <m.i.toaha14@gmail.com>
2023-08-21 17:30:56 +0600

    Typographi and portfolio section create

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import './intro.scss'
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped'
+import './intro.scss';
 const Intro = () => {
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay:1500,
+            backSpeed:60,
+            strings: ['Designer','Developer','Content Creator']
+        })
+
+    }, []);
     return (
         <div className='intro' id='intro'>
-            
+            <div className="left">
+                <div className="imageContainer">
+                    <img src='assets/me.png' alt="" />
+                </div>
+
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Md. Toha</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+
         </div>
     );
 };
--- src/components/Portfolio/Portfoio.jsx
+++ src/components/Portfolio/Portfoio.jsx
@@ -3,7 +3,40 @@ import './portfolio.scss'
 const Protfoio = () => {
     return (
         <div className='portfolio' id='portfolio'>
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+                <div className="item">
+                    <img src="https://static.vecteezy.com/system/resources/previews/005/260/247/original/illustration-graphic-cartoon-character-of-money-saving-of-bank-free-vector.jpg" alt="" />
+                    <h4>Banking App</h4>
+                </div>
+            </div>
         </div>
     );
 };
