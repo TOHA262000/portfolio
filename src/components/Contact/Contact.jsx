4a8aacb95c629991b815b483b7c569ff371e0f7a
TOHA262000 <m.i.toaha14@gmail.com>
2023-08-24 15:29:02 +0600

    Contact page finish

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,18 @@ import './contact.scss'
 const Contact = () => {
     return (
         <div className='contact' id='contact'>
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h1>Contact.</h1>
+                <form action="">
+                    
+                    <input  type="email" placeholder='Email' />
+                    <textarea placeholder='Message' id="" cols="30" rows="10"></textarea>
+                    <input className='sendBtn' type="submit" value="Send" />
+                </form>
+            </div>
             
         </div>
     );
